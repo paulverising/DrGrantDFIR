93a4ccca10949fc775e37ad0c2d4fa84ffb350f8
Zach Paul <zachpaul.21@gmail.com>
2022-10-22 12:11:57 -0700

    Updated the models

--- DrGrant/cases/admin.py
+++ DrGrant/cases/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Case, System_Type, System, Task_Status, Task, Account_Type, Account
 
-# Register your models here.
+admin.site.register(Case)
+admin.site.register(System_Type)
+admin.site.register(System)
+admin.site.register(Task_Status)
+admin.site.register(Task)
+admin.site.register(Account_Type)
+admin.site.register(Account)
+
+
--- DrGrant/cases/models.py
+++ DrGrant/cases/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.forms import CharField, DateTimeField
 from django.contrib.auth.models import User
 
-class Cases(models.Model):
+class Case(models.Model):
     name = models.CharField(max_length=200)
     dateCreated = models.DateTimeField(auto_now=False, auto_now_add=True)
     dateModified = models.DateTimeField(auto_now=True, auto_now_add=False)
@@ -11,24 +11,49 @@ class Cases(models.Model):
     initialAccess = models.DateTimeField(editable=True)
     firstDetectTime = models.DateTimeField(editable=True)
 
-class InScopeSystems(models.Model):
-    case = models.ForeignKey(Cases, default=1, on_delete=models.SET_DEFAULT)
-    name = models.CharField(max_length=200)
+class System_Type(models.Model):
+    type = models.CharField(max_length=200)
     dateCreated = models.DateTimeField(auto_now=False, auto_now_add=True)
     dateModified = models.DateTimeField(auto_now=True, auto_now_add=False)
-    description = models.TextField()
 
-class Tasks(models.Model):
-    case = models.ForeignKey(Cases, default=1, on_delete=models.SET_DEFAULT)
+class System(models.Model):
+    case = models.ForeignKey(Case, default=1, on_delete=models.SET_DEFAULT)
     name = models.CharField(max_length=200)
     dateCreated = models.DateTimeField(auto_now=False, auto_now_add=True)
     dateModified = models.DateTimeField(auto_now=True, auto_now_add=False)
     description = models.TextField()
-    system = models.ForeignKey(InScopeSystems, default=1, on_delete=models.SET_DEFAULT)
+    is_Attacker = models.BooleanField()
+    system_type = models.ForeignKey(System_Type, default=1, on_delete=models.SET_DEFAULT)
+
+class Account_Type(models.Model):
+    type = models.CharField(max_length=200)
+    dateCreated = models.DateTimeField(auto_now=False, auto_now_add=True)
+    dateModified = models.DateTimeField(auto_now=True, auto_now_add=False)
+    
+class Account(models.Model):
+    case = models.ForeignKey(Case, default=1, on_delete=models.SET_DEFAULT)
+    name = models.CharField(max_length=200)
+    dateCreated = models.DateTimeField(auto_now=False, auto_now_add=True)
+    dateModified = models.DateTimeField(auto_now=True, auto_now_add=False)
+    description = models.TextField()
+    is_AttackerCreated = models.BooleanField()
+    account_type = models.ForeignKey(Account_Type, default=1, on_delete=models.SET_DEFAULT)
+    
 
 class Task_Status(models.Model):
     status = models.CharField(max_length=200)
     dateCreated = models.DateTimeField(auto_now=False, auto_now_add=True)
     dateModified = models.DateTimeField(auto_now=True, auto_now_add=False)
+    
+class Task(models.Model):
+    case = models.ForeignKey(Case, default=1, on_delete=models.SET_DEFAULT)
+    name = models.CharField(max_length=200)
+    dateCreated = models.DateTimeField(auto_now=False, auto_now_add=True)
+    dateModified = models.DateTimeField(auto_now=True, auto_now_add=False)
+    description = models.TextField()
+    system = models.ForeignKey(System, default=1, on_delete=models.SET_DEFAULT)
+    task_status = models.ForeignKey(Task_Status, default=1, on_delete=models.SET_DEFAULT)
+    assignee = models.ForeignKey(User, default="User Deleted", on_delete=models.SET_DEFAULT)
+
 
 
