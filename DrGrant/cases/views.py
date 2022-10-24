525681db3f999e73752e5c7c8b2dbfc1a6121d7e
Zach Paul <zachpaul.21@gmail.com>
2022-10-23 20:19:05 -0700

    forgot to stage

--- DrGrant/cases/views.py
+++ DrGrant/cases/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .models import Case
 
+@login_required
 def cases(request):
-    return render(request, 'cases/cases.html')
+    context = {
+        'cases': Case.objects.all()
+    }
+    return render(request, 'cases/cases.html', context)
